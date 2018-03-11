778cd5bbd9864748c97e743553005838b585f519
snotlingorc <snotling@gmail.com>
2018-03-11 11:47:11 -0600

    fixing _uploaded path

--- data/_site/error_data.php
+++ data/_site/error_data.php
@@ -10,26 +10,46 @@ $file_stats = array (
 );
 
 $error_data = array (
-  '404_TEXT' => '<table>
-	<tbody>
-		<tr>
-			<td><img alt="" src="/data/_uploaded/image/deathroller1.jpg" style="width: 200px; height: 194px;" /></td>
-			<td>
-			<h2>The page must have been eaten by the snotlings!</h2>
-
-			<p>Or at least never existed at all. &nbsp;I mean a page that was eaten by the snotlings are not refundable, nor do we think you really want them back anyways.</p>
-
-			<p>We would have to catch the little bugger, and hope we get the right one. Gut &#39;em, hope that the file still exists and put it back up. I mean snotling goo all over the interwebs, not a fun thing to clean up after, no really.</p>
-			</td>
-		</tr>
-	</tbody>
-</table>
-
-<p><b>Try some of these out : </b></p>
-
-<div class="404_suggestions">{{Similar_Titles}}</div>
-
-<p>&nbsp;</p>
+  '404_TEXT' => '<table>
+
+	<tbody>
+
+		<tr>
+
+			<td><img alt="" src="/data/uploaded/image/deathroller1.jpg" style="width: 200px; height: 194px;" /></td>
+
+			<td>
+
+			<h2>The page must have been eaten by the snotlings!</h2>
+
+
+
+			<p>Or at least never existed at all. &nbsp;I mean a page that was eaten by the snotlings are not refundable, nor do we think you really want them back anyways.</p>
+
+
+
+			<p>We would have to catch the little bugger, and hope we get the right one. Gut &#39;em, hope that the file still exists and put it back up. I mean snotling goo all over the interwebs, not a fun thing to clean up after, no really.</p>
+
+			</td>
+
+		</tr>
+
+	</tbody>
+
+</table>
+
+
+
+<p><b>Try some of these out : </b></p>
+
+
+
+<div class="404_suggestions">{{Similar_Titles}}</div>
+
+
+
+<p>&nbsp;</p>
+
 ',
 );
 
